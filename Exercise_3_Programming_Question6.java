b26c5293a51b8f904ef0758adc5049f7c3907e1e
GokulNath20 <61192087+GokulNath20@users.noreply.github.com>
2020-03-07 01:46:56 +0800

    Add files via upload

---
+++ Exercise_3_Programming_Question6.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Exercise_3_Programming_Question6 {
+    public static void main(String[] args){
+
+        Scanner gk = new Scanner(System.in);
+        System.out.print("Enter value of x: ");
+        double x = gk.nextDouble();
+        System.out.print("Enter value of y: ");
+        double y = gk.nextDouble();
+
+        System.out.println(Math.abs(x));
+    }
+}
